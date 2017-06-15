20a05f3c818f71ca6ee0242332d93030ea33f5da
LANCASTER\bstinnett <bstinnett@webstaurantstore.com>
2017-06-15 09:44:34 -0400

    Add project files.

---
+++ Epicenter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Epicenter.Startup))]
+namespace Epicenter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
